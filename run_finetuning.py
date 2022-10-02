d525e778d0a5163e11269234ab86ea83445b986d
OrigamiDream <sdy36071@naver.com>
2022-10-02 09:34:00 +0900

    Update Adam optimizer selection

--- run_finetuning.py
+++ run_finetuning.py
@@ -258,14 +258,19 @@ def run_train(strategy, config, train_dataset, valid_dataset, steps_per_epoch):
     if config.layerwise_lr_decay:
         layer_decay = _get_layer_decay(config.layerwise_lr_decay, config.pretrained_config.num_hidden_layers)
 
-    optimizer = AdamWeightDecay(
-        learning_rate=learning_rate_fn,
-        weight_decay_rate=config.weight_decay,
-        layer_decay=layer_decay,
-        epsilon=1e-6,
-        exclude_from_weight_decay=['layer_norm', 'bias', 'LayerNorm'],
-        clip_norm=config.optimizer_clip_norm
-    )
+    if config.weight_decay > 0.0:
+        optimizer = AdamWeightDecay(
+            learning_rate=learning_rate_fn,
+            weight_decay_rate=config.weight_decay,
+            layer_decay=layer_decay,
+            exclude_from_weight_decay=['layer_norm', 'bias', 'LayerNorm'],
+            clip_norm=config.optimizer_clip_norm
+        )
+    else:
+        optimizer = optimizers.Adam(
+            learning_rate=learning_rate_fn,
+            clipnorm=config.optimizer_clip_norm
+        )
 
     # Metrics
     def create_metric_map():
